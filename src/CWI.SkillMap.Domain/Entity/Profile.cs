f0776b83838007d93d884779770ae7ee68f97cbf
Alexandre de Lima Machado <alexandre@machado.cc>
2015-06-10 22:11:12 -0300

    feat: cria projeto base

---
+++ src/CWI.SkillMap.Domain/Entity/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CWI.SkillMap.Domain.Entity
+{
+    public class Profile
+    {
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Skill> Skills { get; set; }
+    }
+}
---
+++ src/CWI.SkillMap.Domain/Entity/Skill.cs
@@ -0,0 +1,11 @@
+﻿namespace CWI.SkillMap.Domain.Entity
+{
+    public class Skill
+    {
+        public string SkillName { get; set; }
+
+        public string Image { get; set; }
+
+        public string Group { get; set; }
+    }
+}
